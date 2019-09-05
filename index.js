63a17cb0a9bf0214379a88e3118024fdb2167c4c
nionata <n.ionata129@gmail.com>
2019-09-05 16:36:41 -0400

    finished crud planet implementation

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var express = require('express') // get the express dependency
-var app = express() // init express
-var planets = ['earth']
+const express = require('express') // get the express dependency
+const bodyParser = require('body-parser') // get the body parser
+let app = express() // init express
+let planets = ['earth'] // new planets array
+
+// validator function
+const validateRequest = (id, planet) => {
+    if (id) {
+        if (!isNaN(id)) {
+            if (planet) {
+                return true
+            } else {
+                return 'Planet not found'
+            }
+        } else {
+            return 'Id is not a number'
+        }
+    } else {
+        return 'Missing id param'
+    }
+}
+
+// parse application/json
+app.use(bodyParser.json()) 
 
 // add middleware
 app.use((req, res, next) => {
@@ -13,28 +34,62 @@ app.get('/hello', (req, res, next) => {
     res.send('world')
 })
 
+// handle post reqs for /planets
+app.post('/planets', (req, res) => {
+    const planet = req.body.planet
+
+    if (planet) {
+        planets.push(req.body.planet)
+        res.json(planets)
+    } else {
+        res.send('Missing required planet param')
+    }
+})
+
 // handle get reqs for /planets
 app.get('/planets', (req, res) => {
     res.json(planets)
 })
 
-// handle post reqs for /planets
-app.post('/planetz', (req, res) => {
-    console.log(req.body);
-    
-    planets.push(req.body.planet)
-    res.json(planets)
-})
-
 // handle get reqs for /planets/:id
 app.get('/planets/:id', (req, res) => {
-    const planet = planets[req.params.id]
+    const id = req.params.id
+    const planet = planets[id]
+    const validRequest = validateRequest(id, planet)
 
-    if (planet) {
+    if (validRequest === true) {
         res.send(planet)
     } else {
-        res.send('Invalid planet id')
+        res.send(validRequest)
     }
+})
+
+// handle put reqs for /planets/:id
+app.put('/planets/:id', (req, res) => {
+    const id = req.params.id
+    const planet = req.body.planet
+    const validRequest = validateRequest(id, planets[id])
+
+    if (validRequest === true) {
+        planets[id] = planet
+        res.send(planets)
+    } else {
+        res.send(validRequest)
+    }
+})
+
+// handle delete reqs for /planets/:id
+app.delete('/planets/:id', (req, res) => {
+    const id = req.params.id
+    const planet = planets[id]
+    const validRequest = validateRequest(id, planet)
+
+    if (validRequest === true) {
+        planets.splice(id, 1)
+        res.send(planets)
+    } else {
+        res.send(validRequest)
+    } 
 })
 
 // start app on port 3000
